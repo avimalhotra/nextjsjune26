8968fb96b7c126f100e167bb32d8668e0cfdde71
Avinash Malhotra <avimalhotra505@gmail.com>
2026-06-20 12:32:07 +0530

    installation

---
+++ app/contact/page.tsx
@@ -0,0 +1,16 @@
+import Footer from "../footer";
+
+export default function About() {
+  return (
+    <div className="container mx-auto">
+          <header>
+            <h1 className="text-4xl font-bold">Contact Us</h1>
+          </header>
+            <main>
+               <p className="my-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ipsa iure aut dolore, quia laborum, nulla eum nobis voluptates est sint consequatur, repellendus debitis eveniet officia adipisci quis libero. Quod.</p>
+              <p className="my-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ipsa iure aut dolore, quia laborum, nulla eum nobis voluptates est sint consequatur, repellendus debitis eveniet officia adipisci quis libero. Quod.</p>
+            </main>
+           <Footer></Footer>
+    </div>
+  );
+}
---
+++ app/footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer(){
+     return (
+          <footer className="p-2 flex justify-between">
+               <p>&copy; 2027</p>
+               <p>All Rights Reserved</p>
+          </footer>
+     )
+}
